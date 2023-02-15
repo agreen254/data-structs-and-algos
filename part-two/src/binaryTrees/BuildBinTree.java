b4c79bf1aad6130385d04bb245160361735e26e9
Aaron Green <aarongreen1997@gmail.com>
2023-02-15 03:31:45 -0600

    begin binary trees

---
+++ part-two/src/binaryTrees/BuildBinTree.java
@@ -0,0 +1,11 @@
+package binaryTrees;
+
+public class BuildBinTree {
+    // Tree (root)
+    // Node (value, leftChild, rightChild)
+    // insert(int value)
+    // find(int value) : boolean
+
+    // var current = root;
+    // if insert value < root go left current = current.leftChild, else go right
+}
